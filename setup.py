b93ca9219227eb450043b45eee8f005723d2d0f6
Pedro Brochado <pedropsb95@gmail.com>
2024-03-20 18:11:42 -0300

    Fix for setup.py failure
    [noissue]

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="pulp-docs",
-    version="0.0.1",
-    description="Description of my package",
-    packages=find_packages(),
-)
+setup()
